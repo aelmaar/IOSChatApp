ed419cf8aef0eb77d0d0174a52209e360ddb6b4b
Anouar El Maaroufi <anouar@Anouars-MacBook-Pro.local>
2024-11-07 19:34:11 +0100

    feat(users): Setup the backend to handle OAuth flow with 42 school intranet

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,4 @@
-from .views import RegisterView, LoginView, OAuthGoogleCallbackView, OAuthGoogleLoginView
+from .views import RegisterView, LoginView, OAuthGoogleCallbackView, OAuth42CallbackView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
     path('oauth/google/callback/', OAuthGoogleCallbackView.as_view(), name='oauth-google-callback'),
+    path('oauth/42/callback/', OAuth42CallbackView.as_view(), name='oauth-42-callback'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -16,6 +16,8 @@ from users.models import Users
 from django.utils.crypto import get_random_string
 import requests
 import logging
+import random
+import string
 
 logger = logging.getLogger(__name__)
 
@@ -46,11 +48,12 @@ class LoginView(APIView):
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class OAuthGoogleCallbackView(APIView):
     permission_classes = [IsUnauthenticated]
 
     def post(self, request):
-        authorization_code = request.data.get('code')
+        authorization_code = request.data.get("code")
 
         if not authorization_code:
             raise ValidationError(
@@ -90,8 +93,14 @@ class OAuthGoogleCallbackView(APIView):
     def get_or_create_user(self, userinfo):
         email = userinfo.get("email")
         user = Users.objects.filter(email=email).first()
+        username = f"{userinfo.get('given_name').lower()}_{userinfo.get('id')[:5]}"
+
         if not user:
-            username = f"{userinfo.get('given_name').lower()}_{userinfo.get('id')[:5]}"
+            base_username = username
+            while Users.objects.filter(username=username).exists():
+                random_id = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+                username = f"{base_username}_{random_id}"
+
             password = get_random_string(30)
             user = Users.objects.create_user(
                 username=username,
@@ -123,6 +132,96 @@ class OAuthGoogleCallbackView(APIView):
         response = requests.get(
             "https://www.googleapis.com/oauth2/v2/userinfo", headers=headers
         )
+        if not response.ok:
+            return None
+        return response.json()
+
+class OAuth42CallbackView(APIView):
+    permission_classes = [IsUnauthenticated]
+
+    def post(self, request):
+        authorization_code = request.data.get("code")
+
+        logger.info(f"Authorization code: {authorization_code}")
+        if not authorization_code:
+            raise ValidationError(
+                {
+                    "error_message": "Authorization code is missing. Please try logging in again."
+                }
+            )
+
+        access_token = self.exchange_code_with_access_token(authorization_code)
+        if access_token is None:
+            raise ValidationError(
+                {
+                    "error_message": "The authorization code is invalid or expired. Please try logging in again."
+                }
+            )
+
+        userinfo = self.request_42_userinfo(access_token)
+        if userinfo is None:
+            raise ValidationError(
+                {
+                    "error_message": "Authentication failed. Please ensure you are logged in and try again."
+                }
+            )
+
+        user = self.get_or_create_user(userinfo)
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "message": "Login successful",
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    def get_or_create_user(self, userinfo):
+        email = userinfo.get("email")
+        username = userinfo.get("login")
+        user_id = userinfo.get("id")
+        user = Users.objects.filter(email=email).first()
+
+        if not user:
+            base_username = username
+            password = get_random_string(30)
+            picture = userinfo.get("image").get("versions").get("medium")
+
+            while Users.objects.filter(username=username).exists() or Users.objects.filter(username=f"{username}_{user_id}"):
+                random_id = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+                username = f"{base_username}_{random_id}"
+            
+            user = Users.objects.create_user(
+                username=username,
+                email=email,
+                first_name=userinfo.get("first_name"),
+                last_name=userinfo.get("last_name"),
+                picture=picture,
+                password=password
+            )
+        return user
+
+    def exchange_code_with_access_token(self, authorization_code):
+        data = {
+            "code": authorization_code,
+            "redirect_uri": settings.REDIRECT_42_URI,
+            "client_id": settings.CLIENT_42_ID,
+            "client_secret": settings.CLIENT_42_SECRET,
+            "grant_type": "authorization_code",
+        }
+
+        response = requests.post(settings.TOKEN_42_URI, data)
+        response_data = response.json()
+
+        return response_data.get("access_token", None)
+
+    def request_42_userinfo(self, access_token):
+        headers = {"Authorization": f"Bearer {access_token}"}
+
+        response = requests.get("https://api.intra.42.fr/v2/me", headers=headers)
+
         if not response.ok:
             return None
         return response.json()
