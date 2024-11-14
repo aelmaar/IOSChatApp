b00cc55cd8cff11c53718aa43377cd1bfb9e4d8c
Anouar El Maaroufi <anouar@Anouars-MacBook-Pro.local>
2024-11-14 16:29:24 +0100

    fix(users): Fix KeyError of birthdate field when retrieve it from validated_data on 'RegisterSerializer'

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -82,7 +82,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             username=validated_data["username"],
             first_name=validated_data["first_name"],
             last_name=validated_data["last_name"],
-            birthdate=validated_data["birthdate"],
+            birthdate=validated_data.get("birthdate"),
         )
         user.set_password(validated_data["password"])
         user.picture = validated_data.get("picture")
