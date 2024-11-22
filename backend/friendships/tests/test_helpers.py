5b890cc9d7f91b180b85d5929324ca312a38aef5
Anouar El Maaroufi <anouar@Anouars-MacBook-Pro.local>
2024-11-22 13:08:13 +0100

    style(friendships): Create test_helpers.py in tests directory within the friendships app and put 'FriendshipsTestsBase' class in it to separate helper classes or functions

---
+++ backend/friendships/tests/test_helpers.py
@@ -0,0 +1,28 @@
+from chat_app.helpers import create_test_user, get_auth_headers
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+class FriendshipsTestsBase(TestCase):
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = create_test_user(username="testuser", email="testuser@example.com")
+        self.url = "/api/friendships/"
+        self.another_user = create_test_user(
+            username="anotheruser", email="anotheruser@example.com"
+        )
+        self.headers = get_auth_headers(self.client, "testuser", "Swift-1234")
+
+    def create_friendship(self, user1=None, user2=None):
+        user1 = user1 or self.user
+        user2 = user2 or self.another_user
+        return Friendships.objects.create(user1=user1, user2=user2)
+
+    def get_headers_for_unauthorized_user(self):
+        create_test_user(
+            username="unauthorizeduser", email="unauthorizeduser@example.com"
+        )
+
+        headers = get_auth_headers(self.client, "unauthorizeduser", "Swift-1234")
+
+        return headers
--- backend/friendships/tests/test_views.py
+++ backend/friendships/tests/test_views.py
@@ -1,39 +1,11 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
 from rest_framework import status
-from django.http import Http404
 from friendships.models import Friendships
 from chat_app.helpers import create_test_user, get_auth_headers
 from users.serializers import UsersSerializer
+from .test_helpers import FriendshipsTestsBase
 
 Users = get_user_model()
-
-
-class FriendshipsTestsBase(TestCase):
-
-    def setUp(self) -> None:
-        self.client = APIClient()
-        self.user = create_test_user(username="testuser", email="testuser@example.com")
-        self.url = "/api/friendships/"
-        self.another_user = create_test_user(
-            username="anotheruser", email="anotheruser@example.com"
-        )
-        self.headers = get_auth_headers(self.client, "testuser", "Swift-1234")
-
-    def create_friendship(self, user1=None, user2=None):
-        user1 = user1 or self.user
-        user2 = user2 or self.another_user
-        return Friendships.objects.create(user1=user1, user2=user2)
-
-    def get_headers_for_unauthorized_user(self):
-        create_test_user(
-            username="unauthorizeduser", email="unauthorizeduser@example.com"
-        )
-
-        headers = get_auth_headers(self.client, "unauthorizeduser", "Swift-1234")
-
-        return headers
 
 
 class CreateFriendshipsViewTests(FriendshipsTestsBase):
