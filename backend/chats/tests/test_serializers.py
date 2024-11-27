30caf54887c47def9fa1ccce4a8847993e9e733c
Anouar El Maaroufi <anouar@Anouars-MacBook-Pro.local>
2024-11-27 19:21:46 +0100

    test(chats): Write unit tests for 'MessagesSerializer'

--- backend/chats/tests/test_serializers.py
+++ backend/chats/tests/test_serializers.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from unittest.mock import Mock
 from chat_app.helpers import create_test_user
-from chats.serializers import ConversationsSerializer
+from chats.serializers import ConversationsSerializer, MessagesSerializer
 from django.http import Http404
 from chats.models import Conversations, Messages
+from rest_framework.serializers import ValidationError
 
 
 class ConversationsSerializerTests(TestCase):
@@ -94,3 +95,103 @@ class ConversationsSerializerTests(TestCase):
 
         self.assertTrue(serializer.data["IsBlockedByMe"])
         self.assertTrue(serializer.data["IsBlockedByOtherUser"])
+
+
+class MessagesSerializerTests(TestCase):
+
+    def setUp(self) -> None:
+        self.user = create_test_user(username="testuser", email="testuser@example.com")
+        another_user = create_test_user(
+            username="anotheruser", email="anotheruser@example.com"
+        )
+
+        # Create a conversation
+        self.conversation = Conversations.objects.create(
+            user1=self.user, user2=another_user
+        )
+
+        self.mock_request = Mock()
+        self.mock_request.user = self.user
+        self.mock_request.query_params = {"conversation_id": self.conversation.id}
+
+    def test_content_field_validation(self):
+        test_cases = [
+            {
+                "input": "Hello, this is a normal message",
+                "expected": "Hello, this is a normal message",
+            },
+            {
+                "input": "Line 1\nLine 2",
+                "expected": "Line 1\nLine 2",
+            },
+            {
+                "input": "<script>alert('xss')</script>",
+                "expected": "&lt;script&gt;alert(&#x27;xss&#x27;)&lt;/script&gt;",
+            },
+            {
+                "input": "<img src='malicious.jpg'/>",
+                "expected": "&lt;img src=&#x27;malicious.jpg&#x27;/&gt;",
+            },
+            {
+                "input": "Hello 世界 🎉",
+                "expected": "Hello 世界 🎉",
+            },
+            {
+                "input": "    ",
+                "expected": "    ",
+            },
+            # Invalid cases
+            {
+                "input": None,
+                "error": "This field may not be null.",
+            },
+            {
+                "input": "",
+                "error": "This field may not be blank.",
+            },
+            {
+                "input": {"key": "value"},
+                "error": "Not a valid string.",
+            },
+        ]
+
+        for case in test_cases:
+            with self.subTest():
+                if "error" in case:
+                    serializer = MessagesSerializer(
+                        data={"content": case["input"]},
+                        context={"request": self.mock_request},
+                    )
+                    with self.assertRaises(ValidationError) as cm:
+                        serializer.is_valid(raise_exception=True)
+                    self.assertEqual(
+                        str(cm.exception.detail.get("content")[0]), case["error"]
+                    )
+
+                else:
+                    serializer = MessagesSerializer()
+                    result = serializer.validate_content(case["input"])
+                    self.assertEqual(result, case["expected"])
+
+    def test_nonexistent_conversation(self):
+
+        self.mock_request.query_params = {"conversation_id": 1000}
+        with self.assertRaises(Http404):
+            serializer = MessagesSerializer(
+                data={"content": "Hello Friends!"},
+                context={"request": self.mock_request},
+            )
+
+            self.assertFalse(serializer.is_valid(), msg=serializer.errors)
+
+    def test_success_message_creation(self):
+        serializer = MessagesSerializer(
+            data={"content": "Hello friends!"}, context={"request": self.mock_request}
+        )
+
+        self.assertTrue(serializer.is_valid(), msg=serializer.errors)
+        message = serializer.save()
+        self.assertIsNotNone(message)
+        self.assertEqual(message.content, "Hello friends!")
+        self.assertEqual(message.conversation.id, self.conversation.id)
+        self.assertEqual(message.sender, self.user)
